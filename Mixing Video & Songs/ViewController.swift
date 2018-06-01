846a3a859ba689942ed183015540f76e27f92372
abdalrahman essam <abdalrahmanessam@abdalrahmans-MacBook-Pro.local>
2018-06-01 07:56:52 +0200

    Initial Commit

---
+++ Mixing Video & Songs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mixing Video & Songs
+//
+//  Created by abdalrahman essam  on 6/1/18.
+//  Copyright © 2018 abdalrahman essam . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
